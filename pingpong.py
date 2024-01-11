39720de096358872c567d3f21dadcfe119cec7c1
Minh <93335898+Tuquyk@users.noreply.github.com>
2024-01-11 23:27:46 +0700

    Update pingpong.py
    add 2 player mode

--- pingpong.py
+++ pingpong.py
@@ -8,17 +8,19 @@ def reset_ball():
     ball_speed_y *= random.choice([-1,1])
 
 def point_won(winner):
-    global cpu_points, player_points
+    global cpu_points, player2_points , player1_points 
 
     if winner == "cpu":
         cpu_points += 1
-    if winner == "player":
-        player_points += 1
+    if winner == "player1":
+        player1_points += 1    
+    if winner == "player2":
+        player2_points += 1
 
     reset_ball()
 
 def animate_ball():
-    global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y, mode
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
@@ -26,16 +28,18 @@ def animate_ball():
         ball_speed_y *= -1
 
     if ball.right >= screen_width:
-        point_won("cpu")
-
+        if mode==0:
+            point_won("cpu")
+        if mode==1:
+            point_won("player1")
     if ball.left <= 0:
-        point_won("player")
+        point_won("player2")
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
-    if ball.colliderect(player) or ball.colliderect(cpu):
+    if ball.colliderect(player2) or (ball.colliderect(cpu) and mode==0) or (ball.colliderect(player1) and mode==1):
         ball_speed_x *= -1
         collision.play()
 
-def animate_player():
+def animate_player(player, player_speed):
     player.y += player_speed
 
     if player.top <= 0:
@@ -73,41 +77,31 @@ pygame.init()
 
 screen_width = 800
 screen_height = 600
-collision=pygame.mixer.Sound("resource/colli.wav") # add sound
+collision=pygame.mixer.Sound("pygame/ping pong/resource/colli.wav") # add sound
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("pingpongchingchong") # it's just a joke ( I'm Asian ) 
 clock = pygame.time.Clock()
-
-ball = pygame.Rect(0,0,30,30)
-ball.center = (screen_width/2, screen_height/2)
-
-cpu = pygame.Rect(0,0,20,100)
-cpu.centery = screen_height/2
-
-player = pygame.Rect(0,0,20,100)
-player.midright = (screen_width, screen_height/2)
-
-ball_speed_x = 7
-ball_speed_y = 7
-player_speed = 7
-cpu_speed = 1
-
-cpu_points, player_points = 0, 0
-
 score_font = pygame.font.Font(None, 75)
 
 screen.fill((0,0,0))
 
 #main menu
-button_1=button(50, 100, 300, 50)
-button_2=button(50, 200, 300, 50)
-button_3=button(50, 300, 300, 50)
+button_1=button(50, 100, 375, 50)
+button_2=button(50, 200, 375, 50)
+button_3=button(50, 300, 375, 50)
+button_4=button(50, 400, 375, 50)
+
+#button
 menu=True
 
-mode=1
+control=1
+textcontrol=''
+
 difficulty=0
-textmode=''
 textdifficulty=''
+
+mode=0
+textmode=''
 
 click=False
 
@@ -121,85 +115,125 @@ while menu:
     button_1.draw(192,192,192)
     button_2.draw(192,192,192)
     button_3.draw(192,192,192)
+    button_4.draw(192,192,192)
     pos= pygame.mouse.get_pos()
-    if mode==1:
-        textmode='MOUSE'
-    elif mode==0:
-        textmode='BUTTON'
+    # buttons status
+    if control==1:
+        textcontrol='Mouse'
+    elif control==0:
+        textcontrol='Buttons'
+    
+    if mode==0:
+        textmode='Player vs cpu'
+    elif mode==1:
+        textmode='Player vs player'
+    
     if difficulty==0:
-        textdifficulty='EASY'
+        textdifficulty='Easy'
     elif difficulty==1:
-        textdifficulty='MEDIUM'
+        textdifficulty='Medium'
     elif difficulty==2:
-        textdifficulty='HARD'
+        textdifficulty='Hard'
     elif difficulty==3:
-        textdifficulty='IMPOSSIBLE'
+        textdifficulty='Impossible'
     text('MAIN MENU', pygame.font.Font(None,75), (255, 255, 255), screen, 20, 20)
-    text('START', fbutton, (255, 255, 255), screen, 65, 120)
-    text(('MODE:' + textmode), fbutton, (255, 255, 255), screen, 65, 220)
-    text(('DIFFICULTY:' + textdifficulty), fbutton, (255, 255, 255), screen, 65, 320)
+    text('Start', fbutton, (255, 255, 255), screen, 65, 120)
+    text(('Control:' + textcontrol), fbutton, (255, 255, 255), screen, 65, 220)
+    text(('Difficulty:' + textdifficulty), fbutton, (255, 255, 255), screen, 65, 320)
+    text(('Mode:' + textmode), fbutton, (255, 255, 255), screen, 65, 420)
     if pygame.mouse.get_pressed()[0] == 1 and click==False:
         if button_1.rectangle.collidepoint(pos):
             menu=False
         elif button_2.rectangle.collidepoint(pos):
-            mode=1-mode
+            control=1-control
             click=True
         elif button_3.rectangle.collidepoint(pos):
             difficulty=difficulty+1
             if difficulty==4:
                 difficulty=difficulty-4
-            cpu_speed = 7-((3-difficulty)*2)
+            cpu_speed = 7-((3-difficulty)*2)+2
             click=True
+        elif button_4.rectangle.collidepoint(pos):
+            mode=1-mode
+            click=True    
     if pygame.mouse.get_pressed()[0] == 0:
         click=False
-    
+    if mode==1:
+        control=0
     pygame.display.update()
-    
+# ???
+cpu_points, player2_points , player1_points  = 0, 0 , 0
+ball = pygame.Rect(0,0,30,30)
+ball.center = (screen_width/2, screen_height/2)
+
+cpu = pygame.Rect(0,0,20,100)
+cpu.centery = screen_height/2
+
+player1 = pygame.Rect(0,0,20,100)
+player1.centery = screen_height/2
+
+player2 = pygame.Rect(0,0,20,100)
+player2.midright = (screen_width, screen_height/2)
+
+
+ball_speed_x = 7
+ball_speed_y = 7
+player2_speed = 7
+player1_speed =  7
+cpu_speed = 1
+
+
 run=True
 quit=False
 #ping pong game
 while run:
     #Check if anyone win
-    if cpu_points==5 or player_points==5:
+    if cpu_points==999 or player2_points==999 or player1_points==999:
         run=False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             quit = True
-        if mode==0:
+        if control==0:
             if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    player1_speed = -abs(player1_speed)
+                if event.key == pygame.K_s:
+                    player1_speed = abs(player1_speed)  
                 if event.key == pygame.K_UP:
-                    player_speed = -abs(player_speed)
+                    player2_speed = -abs(player2_speed)
                 if event.key == pygame.K_DOWN:
-                    player_speed = abs(player_speed)
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_UP:
-                        player_speed = 0
-                    if event.key == pygame.K_DOWN:
-                        player_speed = 0
+                    player2_speed = abs(player2_speed)
     #Make paddle parallel with mouse ( if mouse)
-    if mode==1:
-        player = pygame.Rect((screen_width-20,pygame.mouse.get_pos()[1]),(20,100))
-    if mode==0:
-        pygame.Rect(0,0,20,100)
+    if control==1:
+        player2 = pygame.Rect((screen_width-20,pygame.mouse.get_pos()[1]),(20,100))
     #Change the positions of the game objects
     animate_ball()
-    animate_player()
-    animate_cpu()
+    animate_player(player2, player2_speed)
+    if mode==0:
+        animate_cpu()
+    if mode==1:
+        animate_player(player1, player1_speed)
+    
         
     #Clear the screen
     screen.fill((0,128,0))
 
     #Draw the score
-    cpu_score_surface = score_font.render(str(cpu_points), True, "white")
-    player_score_surface = score_font.render(str(player_points), True, "white")
-    screen.blit(cpu_score_surface,(screen_width/4,20))
-    screen.blit(player_score_surface,(3*screen_width/4,20))
+    if mode==0:
+        cpu_score_surface = score_font.render(str(cpu_points), True, "white")
+        screen.blit(cpu_score_surface,(screen_width/4,20))
+        pygame.draw.rect(screen,'white',cpu)
+    if mode==1:
+        player1_score_surface = score_font.render(str(player1_points), True, "white")
+        screen.blit(player1_score_surface,(screen_width/4,20))
+        pygame.draw.rect(screen,'white',player1)
+    player2_score_surface = score_font.render(str(player2_points), True, "white")
+    screen.blit(player2_score_surface,(3*screen_width/4,20))
 
     #Draw the game objects
     pygame.draw.aaline(screen,'white',(screen_width/2, 0), (screen_width/2, screen_height))
-    pygame.draw.rect(screen,'white',cpu)
-    pygame.draw.rect(screen,'white',player)
+    pygame.draw.rect(screen,'white',player2)
     pygame.draw.circle(screen,(192,192,192), (screen_width/2, screen_height/2), 100)
     pygame.draw.ellipse(screen,'white',ball)
 
@@ -209,10 +243,16 @@ while run:
 #result
 if quit==False:
     winner=''
-    if(cpu_points > player_points):
-        winner="CPU IS THE WINNER"
-    if(cpu_points < player_points):
-        winner="HOOMAN IS THE WINNER"
+    if mode==0:
+        if(cpu_points > player2_points):
+            winner="CPU IS THE WINNER"
+        if(cpu_points < player2_points):
+            winner="HOOMAN IS THE WINNER"
+    if mode==1:
+        if(player1_points > player2_points):
+            winner="PLAYER1 IS THE WINNER"
+        if(player1_points < player2_points):
+            winner="PLAYER2 IS THE WINNER"
     end_font=pygame.font.Font(None,25)
     end=True
     while end==True:
@@ -220,7 +260,7 @@ if quit==False:
             if event.type == pygame.QUIT:
                 end = False
         Truewinner = score_font.render(winner, True , (0,0,0))
-        screen.blit((Truewinner),(screen_width/2-225, screen_height/2))
+        screen.blit((Truewinner),(screen_width/2-300, screen_height/2))
         pygame.display.update()
 pygame.quit()
 sys.exit()
